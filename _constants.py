4e033bbc0a427e29c11318fae0879c0e0fc4f33e
rybafish <rybafishcharts@gmail.com>
2022-05-27 14:53:53 +0300

    ignore

--- _constants.py
+++ _constants.py
@@ -1,4 +1,5 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2022-05-06 22:09:53"
-version = "0.91 beta II"
+build_date = "2022-05-27 14:50:04"
+version = "0.91 beta III"
+isbeta = True
