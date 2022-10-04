6bb48e020672de8783da202d5be00ff8ff72cfda
unknown <rybafishcharts@gmail.com>
2022-10-04 19:06:35 +0400

    sync

--- _constants.py
+++ _constants.py
@@ -1,5 +1,5 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2022-10-03 18:41:00"
-version = "0.92 beta I"
+build_date = "2022-10-04 19:05:23"
+version = "0.92 beta II"
 isbeta = True
