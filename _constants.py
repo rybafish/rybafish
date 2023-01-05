92d0b4977a30f8b751179cb3f45f7f20c80ca3a6
Igor Ekimov <igor.ekimov@sap.com>
2023-01-05 18:43:51 +0100

    Back to normality build (minor change)

--- _constants.py
+++ _constants.py
@@ -1,6 +1,6 @@
 # generated automaticaly based on myryba.spec
 
-build_date = "2023-01-05 18:35:57"
+build_date = "2023-01-05 18:43:29"
 version = "0.91 beta IV - SAP Edition (hdbcli)"
 isbeta = True
 platform = "Windows"
