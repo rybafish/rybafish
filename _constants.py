e2a52bb1f5443e34a15fbbc4988f74225f793ebe
unknown <rybafishcharts@gmail.com>
2023-05-22 12:35:44 +0400

    alerting logging, #688

--- _constants.py
+++ _constants.py
@@ -1,6 +1,6 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2023-05-18 14:35:09"
+build_date = "2023-05-22 09:39:15"
 version = "0.93 beta II"
 platform = "Windows"
 isbeta = True
