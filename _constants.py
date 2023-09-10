0cea88582639e4b09be203b2bc1b9d9e0e0b8c53
unknown <rybafishcharts@gmail.com>
2023-09-10 19:48:54 +0200

    SAP pyhdb driver warning, #877

--- _constants.py
+++ _constants.py
@@ -1,6 +1,6 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2023-08-17 22:33:19"
+build_date = "2023-08-17 22:33:44"
 version = "0.95 Ishkhan"
 platform = "Windows"
 isbeta = False
