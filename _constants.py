00394d9c5ab38a187d11aaa0fdb2c15b7c4348ef
unknown <rybafishcharts@gmail.com>
2024-03-04 20:36:31 +0100

    crash on incorrect QColor parameter #948

--- _constants.py
+++ _constants.py
@@ -1,6 +1,6 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2024-02-27 22:23:16"
+build_date = "2024-03-03 21:21:21"
 version = "0.96 beta II"
 platform = "Windows"
 isbeta = True
