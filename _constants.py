2cea3c5686319b1eec37ef012bf7014c3fefa73e
rybafish <rybafishcharts@gmail.com>
2025-03-07 18:35:03 +0100

    none

--- _constants.py
+++ _constants.py
@@ -1,6 +1,6 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2024-08-21 23:17:32"
+build_date = "2025-03-04 08:16:03"
 version = "0.96 beta IV"
 platform = "Windows"
 isbeta = True
