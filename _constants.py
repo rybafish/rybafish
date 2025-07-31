2448f010e8a4e82489975d8c7f3542f6929f8e2a
unknown <rybafishcharts@gmail.com>
2025-07-31 18:18:11 +0200

    fake

--- _constants.py
+++ _constants.py
@@ -1,6 +1,6 @@
 # getarated automaticaly based on myryba.spec
 
-build_date = "2025-03-04 14:55:52"
+build_date = "2025-07-31 18:17:41"
 version = "0.96 beta IV"
 platform = "Windows"
 isbeta = True
